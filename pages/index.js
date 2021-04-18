b394813868ff9bb5686a99fdc405196add7221bb
mathur619 <prakharmathur619@gmail.com>
2021-04-18 11:46:59 +0530

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,37 @@
-export default function Recipes() {
+import { createClient } from "contentful";
+import RecipeCard from "../components/RecipeCard";
+
+export async function getStaticProps() {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE,
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+  });
+
+  const res = await client.getEntries({ content_type: "recipe" });
+
+  return {
+    props: {
+      recipes: res.items,
+    },
+  };
+}
+
+export default function Recipes({ recipes }) {
+  console.log("🚀 ~ file: index.js ~ line 19 ~ Recipes ~ recipes", recipes);
+
   return (
     <div className="recipe-list">
-      Recipe List
+      {recipes.map((recipe) => (
+        <RecipeCard recipe={recipe} key={recipe.sys.id} />
+      ))}
+
+      <style jsx>{`
+        .recipe-list {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          grid-gap: 20px 60px;
+        }
+      `}</style>
     </div>
-  )
+  );
 }
